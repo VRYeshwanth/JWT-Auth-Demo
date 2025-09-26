77d1420beccc22f1a67102351bf793ca9f29395a
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-26 15:24:54 +0530

    Setup /profile route

--- backend/Routes/protectedRoutes.js
+++ backend/Routes/protectedRoutes.js
@@ -1,11 +1,30 @@
-import pool from "../db.js";
 import express from "express";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 const router = express.Router();
 
-router.post("/profile", async (req, res) => {
-    res.send("Running /api/profile successfully");
+router.get("/profile", async (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token) {
+        res.status(403).json({Error: "Token not Present"})
+    }
+
+    await jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if(err) {
+            res.status(403).json({Error: "Invalid or Expired Token"})
+        }
+        
+        res.status(201).json(
+            {
+                "login": true,
+                data: decoded
+            }
+        )
+    })
 })
 
 export default router;
